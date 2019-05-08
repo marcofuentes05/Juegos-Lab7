a3a17f69ecb81681841927eabc1e0fd64da89cde
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-08 09:23:13 -0600

    Fondo y Personaje

---
+++ New Unity Project/Assets/GenjiS.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenjiS : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    Vector2 force = new Vector2(0, 5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (rb)
+            {
+                rb.AddForce(force,ForceMode2D.Impulse);
+            }
+        }
+    }
+}
