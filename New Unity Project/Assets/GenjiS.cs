9147f136e1b722c1d8e427404cd1c3b2fc900467
Marco Fuentes <marcofuentes1607@gmail.com>
2019-05-08 21:17:55 -0600

    Requisitos Cumplidos

--- New Unity Project/Assets/GenjiS.cs
+++ New Unity Project/Assets/GenjiS.cs
@@ -6,6 +6,10 @@ public class GenjiS : MonoBehaviour
 {
     public Rigidbody2D rb;
     Vector2 force = new Vector2(0, 5);
+    Vector2 desp = new Vector2(1, 0);
+    Sprite sp;
+    public GameObject t; //this
+    bool tienePU; //Es para saber si ya recogió el Power Up
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +21,44 @@ public class GenjiS : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            if (rb)
+            rb.AddForce(force, ForceMode2D.Impulse);
+        }
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb.AddForce(desp, ForceMode2D.Impulse);
+            
+            GetComponent<SpriteRenderer>().flipX=false;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.AddForce(-desp, ForceMode2D.Impulse);
+            GetComponent<SpriteRenderer>().flipX = true;
+            
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("coin"))
+        {
+            Destroy(collision.gameObject);
+            tienePU = true;
+        }
+        if (collision.gameObject.CompareTag("enemy"))
+        {
+            if (tienePU)
             {
-                rb.AddForce(force,ForceMode2D.Impulse);
+                Destroy(collision.gameObject);
+            }
+            else
+            {
+                Destroy(t);
             }
         }
     }
